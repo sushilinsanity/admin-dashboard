0032ec166f09d0381a9ea6a7bc8d62ee28f9fcf5
Sushil <sushilmangal731@gmail.com>
2021-02-28 13:49:07 +0530

    completed dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
-    path: '', component: DefaultComponent, children: [
+    path: '', component: DefaultComponent, pathMatch: 'full', children: [
       { path: '', component: DashboardComponent },
     ]
   }
